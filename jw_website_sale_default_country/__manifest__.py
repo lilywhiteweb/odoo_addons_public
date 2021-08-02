279c722de41427417f9f8d5075b51f2244be3feb
John Wiltshire <wiltshire@fb.com>
2021-08-02 18:14:59 +0100

    Added fix to allow module to work on single website apps

--- jw_website_sale_default_country/__manifest__.py
+++ jw_website_sale_default_country/__manifest__.py
@@ -7,6 +7,8 @@
 
     'description': """
         Website Module to allow you to set the default country for a sale on the website.
+
+        V1.1 - Added fix to allow module to work on single website applications
     """,
 
     'author': "Lily White Web",
@@ -18,7 +20,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website',
-    'version': '1.0',
+    'version': '1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','website'],
